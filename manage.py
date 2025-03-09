574787f2fc194ddc68b9317310f7c2dd82b4ebcf
gaganhanglimbu <138977632+gaganhanglimbu@users.noreply.github.com>
2025-03-09 17:01:35 +0545

    Update manage.py

--- manage.py
+++ manage.py
@@ -9,6 +9,18 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+        
+        if username and email and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
+                print("Superuser created successfully.")
+            else:
+                print("Superuser already exists.")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
